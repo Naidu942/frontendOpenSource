a7937bea7c663b0180e9d1fa9435f030365b28af
Naidu942 <subrahmanyamnaidusaladi@gmail.com>
2024-06-11 14:30:56 +0530

    upto loops

---
+++ python/basicPythonClass.py
@@ -0,0 +1,385 @@
+#  single line comments
+
+
+'''
+multi-line comment
+second line
+
+
+'''
+
+
+# variables : are the names of the containers which will hold the data 
+
+#task-1 : find all the restrictions for variable decleration
+
+'''
+data types:
+    1.primitive data types:
+
+        1.intege -inf to +inf  -123453 or 12345
+        2.float  -inf to +inf  -1.23456 or 1.23456
+        3.string  "abcdef" 'abcdef'  "naidu's"
+        4.boolean True or False or 1 or 0
+
+
+
+
+    2. non-primitive data types:
+        1.List
+        2.Dictonary
+        3.set
+        4.tuple
+        5.class
+
+
+
+'''
+
+
+'''
+1.Arithmetic Operators:
+
++
+-
+*
+/
+//---> floor division 
+
+% ->modulo division
+
+2.assignment operators:
+
+=
++=
+-=
+*=
+/=
+//=
+
+3. relational operators:
+
+a == b
+!=
+<
+>
+<=
+>= 
+
+
+
+'''
+
+# num=25
+# string="naidu123345"
+# boolean=True 
+# decimal=34.456
+
+
+# arr=[1,2,3,4]
+
+
+
+# print(type(num))
+# print(type(string))
+# print(type(boolean))
+# print(type(decimal))
+
+
+
+# print(type(arr))
+
+
+
+
+# x=34
+# y=42
+# z=x+y
+
+# print(x+y)
+
+# print(z)
+
+# print(34+42)
+
+
+# conditional statements
+'''
+if 
+elif
+else
+
+'''
+
+# logical operators
+
+
+# if(condition):
+#     # statements
+
+# else:
+#     # statements
+
+
+# switch="onf"   
+
+
+
+# if(switch=="on"):
+#     print("fan is running")
+#     if(switch=="onnn"):
+#         print("")
+#         if(switch=="on"):
+
+    
+
+# if(switch=="on"):
+#     pass
+# elif(switch=="off"):
+#     print("fan is at rest")
+
+# elif(switch=="o"):
+
+#     print("fan is running")
+
+# elif(switch=="of"):
+#     print("fan is at rest!!")
+
+# else:
+#     print("give proper in put")
+
+
+
+
+#input()
+
+
+# name=input("Enter the name: ")
+# age=int(input("Enter the age: "))
+# print(type(age))
+
+# print(f"Welcome {name}")
+# print(f"your age is {age}")
+
+
+
+# a=11
+# b=12
+
+# a=int(12/11)
+# print(a)
+
+
+'''
+ I want to take input from user  
+1.name of the user 
+2.age of the user 
+if the age >=18 
+    I want output eligible for vote 
+else if age is 17 
+    I want output as prepare for voting next time 
+else if age is less than 17 
+    I want output as not eligible to vote 
+other than this number I want enter proper input
+
+'''
+
+# try:
+#     name=input("Enter the name: ")
+#     age=int(input("Enter the age: "))
+
+#     if(age>=18 and age<=150):
+
+#         print(f"Welcome to our website {name} and you can vote")
+
+#     elif(age==17):
+#         print(f"Welcome to our website {name} and prepare for voting next time")
+
+#     elif(age<17 and age>=13):
+#         print(f"Welcome to our website {name} and you are not eligible to vote")
+
+#     else:
+#         print(f"Welcome to our website {name} and enter proper details")
+
+# except:
+#     print("something went wrong")
+
+# 4.logical operators
+# and 
+    # or 
+    # not 
+    # in 
+    # not in 
+    # is 
+
+
+
+# a=20
+# b=3
+
+# print(a/b)
+# print(type(a/b))
+# print(a//b)
+# print(type(a//b))
+
+
+# print(a%b)
+
+
+#  3)20(6
+#     18
+# --------
+#     2
+
+
+# 3*6 +2
+
+
+
+
+#  2)20(10
+#     20
+# --------
+#     0 --> even
+
+
+
+
+#  2)25(12
+#     24
+# --------
+#     1
+
+'''
+dividing with 2
+  0  1
+
+  2   3
+
+  4   5
+
+
+dividing with 7
+
+
+ 0  1  2  3  4  5  6  --> reminders when we divide with 7
+ 7  8  9  10 11 12 13
+ 14 15 16 17 18 19 20 
+ 21 22 23
+
+ 
+
+
+
+ 7) 5(0
+    0
+-------
+    5
+
+7)14(2
+  14
+------
+  0
+
+
+'''
+
+
+# num=int(input("Ente a number: "))
+
+# if(num%2==0):
+#     print(f"{num} is even number")
+
+# else:
+#     print(f"{num} is odd number")
+
+
+
+# loops:
+    # for and while loop
+
+# syntax of basic for:
+
+"""
+for variable in range(1000):  # [lowerbound, upperbound)
+    #statements 
+"""
+
+# for i in range(11):  i=1 i=2 i=3  .....i=10  i=11
+#     print(i)         1    2  3            10  
+
+
+
+# for num in range(1,11): #num=1 num=2  ...11 
+#     if(num%2==0):
+#         print(f"{num} is even number")
+#     else:
+#         print(f"{num} is odd number")
+  
+# print("completed")
+
+
+# syntax of while loop:
+"""
+# variable
+while(condition):
+  # statements
+  #increment/decrement
+
+  
+
+num=1
+
+while(num<=10):  num=1  1
+    print(num)   1       1
+
+"""
+"""
+5. increment/decrement opereators
+  +=number
+  -=number
+
+  num+=1
+
+"""
+# num=1
+
+
+
+# while(num<=10):  
+#     print(num)   
+
+#     num=num+2    
+
+
+# lowerLimit=int(input("Enter your lower limit: "))
+
+# upper_limit=int(input("Enter your upper limit: "))
+
+# while(lowerLimit<=upper_limit):
+#     print(lowerLimit) 
+
+#     lowerLimit+=1
+
+       
+
+
+   
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
